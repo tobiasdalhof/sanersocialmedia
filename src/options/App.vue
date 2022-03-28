29ed8b3ed2321adcc68b97e6487f74296843ea27
Tobi Dalhof <dalhof.tobias@gmail.com>
2022-03-28 23:46:21 +0000

    update text, bump version

--- src/options/App.vue
+++ src/options/App.vue
@@ -2,7 +2,7 @@
   <div class="container max-w-4xl mx-auto p-3">
     <div class="flex items-center border-b border-dark-800 py-5">
       <img src="./images/relieved.svg" alt="Sanity" class="w-7 mr-3" />
-      <div>Sanity - Browse social media with less noise</div>
+      <div>Sanity - Saner Social Media</div>
     </div>
 
     <main class="mt-10">
--- src/services/ElementService.ts
+++ src/services/ElementService.ts
@@ -48,7 +48,7 @@ export default class ElementService {
     footer.setAttribute(QuoteElementDataAttribute.Footer, '')
 
     const optionsLink = document.createElement('a')
-    optionsLink.textContent = 'Sanity Extension'
+    optionsLink.textContent = 'Sanity - Saner Social Media'
     optionsLink.href = getOptionsURL()
     optionsLink.target = '_blank'
     optionsLink.setAttribute(QuoteElementDataAttribute.OptionsLink, '')
