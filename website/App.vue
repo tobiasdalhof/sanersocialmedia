1ea2c314602c1b2c9d9f34e3a80c27f9534e422b
Tobi Dalhof <dalhof.tobias@gmail.com>
2022-05-01 16:35:55 +0000

    improve mobile

--- website/App.vue
+++ website/App.vue
@@ -33,15 +33,15 @@ function generateSiteDescription(site: Site): string {
 <template>
   <header class="flex items-center justify-between p-5 leading-none">
     <div class="flex items-center select-none">
-      <img class="w-8 h-8 mr-4" src="./assets/logo.svg" alt="Logo">
+      <img class="w-5 h-5 mr-2 md:w-8 md:h-8 md:mr-4" src="./assets/logo.svg" alt="Logo">
       <div>
-        <span class="text-lg">Saner Social Media</span>
+        <span class="md:text-lg">Saner Social Media</span>
       </div>
     </div>
     <a :href="gitHubUrl" class="inline-flex items-center select-none" target="_blank">
-      <app-icon :value="mdiGithub" class="w-8 h-8 mr-2" />
+      <app-icon :value="mdiGithub" class="w-5 h-5 mr-1 md:w-8 md:h-8 md:mr-2" />
       <div>
-        <span class="text-lg">Star us on GitHub</span>
+        <span class="md:text-lg">Star us on GitHub</span>
       </div>
     </a>
   </header>
