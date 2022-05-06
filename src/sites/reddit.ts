3916c4d4cf3ae4d129d60f6c569a051580f8b29a
Tobi Dalhof <dalhof.tobias@gmail.com>
2022-05-06 16:53:39 +0000

    add action: hide feed on all subreddits

--- src/sites/reddit.ts
+++ src/sites/reddit.ts
@@ -4,30 +4,53 @@ import Site from '../lib/Site'
 import SiteAction from '../lib/SiteAction'
 import { UserConfigKey } from '../types'
 
+interface CreateHideRedditFeedActionParams {
+  name: string
+  validateUrl: (url: URL) => boolean
+  requiredUserConfigKey: UserConfigKey
+}
+
+function createHideRedditFeedAction(params: CreateHideRedditFeedActionParams): SiteAction {
+  return new SiteAction({
+    name: params.name,
+    validateUrl: params.validateUrl,
+    requiredUserConfigKey: params.requiredUserConfigKey,
+    injectCss: `
+      .scrollerItem,
+      .FohHGMokxXLkon1aacMoi {
+        opacity: 0!important;
+        pointer-events: none!important;
+        width: 0px!important;
+        height: 0px!important;
+      }
+    `,
+    manipulateDom: async ({ siteAction }) => {
+      const container = await waitForElement('.scrollerItem')
+      mute(container)
+      setTimeout(() => {
+        const quote = siteAction.createQuoteElement(container)
+        if (!quote)
+          return
+        container.before(quote)
+      }, 1000)
+    },
+  })
+}
+
 const reddit = new Site({
   logoSvg,
   name: 'Reddit',
   validateUrl: url => url.host.replace('www.', '') === 'reddit.com',
   siteActions: [
-    new SiteAction({
+    createHideRedditFeedAction({
       name: 'Hide feed on home page',
       validateUrl: url => url.pathname === '/',
       requiredUserConfigKey: UserConfigKey.RedditHideHomeFeed,
-      injectCss: `
-        .scrollerItem {
-          display: none!important;
-        }
-      `,
-      manipulateDom: async ({ siteAction }) => {
-        const container = await waitForElement('.scrollerItem')
-        mute(container)
-        setTimeout(() => {
-          const quote = siteAction.createQuoteElement(container)
-          if (!quote)
-            return
-          container.before(quote)
-        }, 1000)
-      },
+    }),
+    createHideRedditFeedAction({
+      name: 'Hide feed on all subreddits',
+      validateUrl: url => url.pathname.includes('/r/'),
+      requiredUserConfigKey: UserConfigKey.RedditHideSubredditsFeed,
     }),
   ],
 })
