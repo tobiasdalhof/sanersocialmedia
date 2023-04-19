ac4dd65bff6d7f4b3ea06fd7d5dd61bf34c6ee05
Tobi Dalhof <dalhof.tobias@gmail.com>
2023-04-19 17:11:31 +0000

    pnpm lint

--- alias.ts
+++ alias.ts
@@ -1,6 +1,8 @@
 import { resolve } from 'node:path'
 
-function r(p: string) { return resolve(__dirname, p) }
+function r(p: string) {
+  return resolve(__dirname, p)
+}
 
 export const alias: Record<string, string> = {
   '@sanersocialmedia/core': r('./packages/core/src/index.ts'),
