b3f6359b0fff2ed9bb41d1f880fd7c2ab369450c
Tobias Dalhof <dalhof.tobias@gmail.com>
2025-04-23 16:32:32 +0200

    update eslint settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import antfu from '@antfu/eslint-config'
-import unocss from '@unocss/eslint-config/flat'
 
-export default antfu(
-  {},
-  unocss,
-)
+export default antfu({
+  unocss: true,
+  vue: true,
+})
